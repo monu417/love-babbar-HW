3f6c28b052bf859dd7f49bb7cced8615ad44a101
Priyanka Kushwaha <98519720+monu417@users.noreply.github.com>
2023-04-05 07:45:51 +0530

    Update solution.cpp

--- recursion-4/question-1/solution.cpp
+++ recursion-4/question-1/solution.cpp
@@ -24,3 +24,35 @@ int ans =mincoin(v,target, mini);
 cout<<ans<<endl;
   return 0;
 }
+
+
+
+
+// second solution
+
+#include <iostream>
+#include<vector>
+#include<limits.h>
+using namespace std;
+int mincoin(vector<int>&v,int target)
+{
+  if(target==0)
+  return 0;
+  if(target<0)
+  return INT_MAX;
+  int mini=INT_MAX;
+  for(int i=0;i<v.size();i++)
+  {
+     int ans=mincoin(v,target-v[i]);
+     if(ans!=INT_MAX)
+     mini=min(mini,ans+1);
+  }
+  return mini;
+}
+int main() {
+int target=12;
+vector<int>v{6,4,5};
+int ans =mincoin(v,target);
+cout<<ans<<endl;
+  return 0;
+}
